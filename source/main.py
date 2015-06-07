d6438604e1cd74ecf55c9346edfb7df8613a1477
Tyler <bulldogboy912000@yahoo.com>
2015-06-07 16:55:55 -0500

    Implemented Project Master/Detail

--- source/main.py
+++ source/main.py
@@ -8,8 +8,12 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.listview import ListView, ListItemButton
 from kivy.adapters.listadapter import ListAdapter
+from kivy.uix.label import Label
+
+from kivy.properties import ObjectProperty, ListProperty, StringProperty
 
 from kivy.uix.splitter import Splitter
 
@@ -55,10 +59,8 @@ Builder.load_string("""
                         "You are in the third tab."))
 
         Splitter:
-            BoxLayout:
+            ProjectDetail:
                 id: project_detail
-                Button:
-                    text: 'Project Detail'
 
 <RootScreen>:
     StackLayout:
@@ -67,45 +69,73 @@ Builder.load_string("""
         ScreenManager:
             MainScreen:
                 id: main_screen
-
-
 """)
 
-projects = ProjectFactory.getProjectsInPath(Settings.PROJECT_DIRECTORY)
 
+class ProjectDetail(GridLayout):
+    
+    project = ObjectProperty()
+    
+    def __init__(self, **kwargs):
+        kwargs['cols'] = 2
+        super(ProjectDetail, self).__init__(**kwargs)
+        self.bind(project = self.redraw)
+        
+    def redraw(self, *args):
+        self.clear_widgets()
+        
+        if self.project:
+            self.add_widget(Label(text="Name", halign = 'right'))
+            self.add_widget(Label(text=self.project.name))
+            
+            for attr_key, attr_value in self.project.__dict__.iteritems():
+                self.add_widget(Label(text=str(attr_key), halign='right'))
+                self.add_widget(Label(text=str(attr_value)))
+
+    def update(self, project):
+        self.project = project
+        self.redraw()
 
 class ProjectsList(ListView):
+    
+    project_list = ListProperty([])
+    
     def __init__(self, **kwargs):
             self.refreshData()
-            #super(ProjectsList, self).__init__(adapter=self.adapter, **kwargs)
-            self.adapter.bind(on_selected_item = self._onSelectItem)
+            super(ProjectsList, self).__init__(adapter=self.adapter, **kwargs)
+            self.adapter.bind(on_selection_change = self._onSelectItem)
 
     def refreshData(self):
+
+        self.project_list = ProjectFactory.getProjectsInPath(Settings.PROJECT_DIRECTORY)
 
         args_converter = lambda row_index, project: {'text': project.name,
                                                      'size_hint_y': None,
                                                      'height': 50}
 
-        self.adapter = ListAdapter(data=projects,
+        self.adapter = ListAdapter(data=self.project_list,
                                    args_converter=args_converter,
                                    cls=ListItemButton,
                                    selection_mode='single',
                                    allow_empty_selection=False)
 
-    def _onSelectItem(self, **kwargs):
-        print self.adapter
-
-    def onSelectProject(self, project):
+    def _onSelectItem(self, adapter):
         '''Set the project detail to the selected project.'''
+        project_index = adapter.selection[0].index
+        project = self.project_list[project_index]
 
+        self.parent.parent.parent.parent.ids.project_detail.update(project)
 
+        #print App.get_running_app().root.ids['main_screen'].ids['project_list'].ids['project_detail_splitter'].ids['project_detail'].update(project)
+
+        
 class RootScreen(BoxLayout):
     pass
 
 
-class MyApp(App):
+class ProjectHelperApp(App):
     def build(self):
         return RootScreen()
 
 if __name__ == "__main__":
-    app = MyApp().run()
+    app = ProjectHelperApp().run()
